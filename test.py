1d5acfdec1931c3310d335863d6298f22fe4bdad
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-18 18:58:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,8 +8,11 @@ api_key = st.text_input("Enter your OpenAI API KEY")
 # 定義 CLI 按鈕
 cli_buttons = [
     {"name": "List of all fine-tunes tasks", "command": f"openai --api-key {api_key} api fine_tunes.list"},
-    {"name": "Delete a fine-tuned model", "command": f"openai --api-key {api_key} api models.delete -i"}
+    {"name": "Delete a fine-tuned model", "command": ""},
 ]
+
+# 定義解析終端輸出的變數
+parsed_output = []
 
 # 執行 CLI 指令
 def execute_command(command):
@@ -17,23 +20,12 @@ def execute_command(command):
     output, error = process.communicate()
     return output.decode("utf-8")
 
-# 解析終端資訊並顯示簡化的資訊表格
+# 解析終端輸出
 def parse_terminal_output(output):
     try:
-        data = json.loads(output)
-        rows = []
-        for i, item in enumerate(data["data"], start=1):
-            row = {
-                "No.": i,
-                "Model Name": item.get("fine_tuned_model"),
-                "Job ID": item.get("id"),
-                "Model": item.get("model"),
-                "Status": item.get("status")
-            }
-            rows.append(row)
-        return rows
-    except Exception as e:
-        st.error(f"Error parsing terminal output: {str(e)}")
+        parsed_output = json.loads(output)
+        return parsed_output["data"]
+    except json.JSONDecodeError:
         return []
 
 # 顯示終端輸出文本區域
@@ -44,23 +36,26 @@ for button in cli_buttons:
     if button["name"] == "List of all fine-tunes tasks":
         if st.button(button["name"]):
             command_output = execute_command(button["command"])
-            terminal_output.text(command_output)
-            
             parsed_output = parse_terminal_output(command_output)
-            st.table(parsed_output)
-
+            table_data = []
+            for index, item in enumerate(parsed_output):
+                table_data.append(
+                    {
+                        "No.": index + 1,
+                        "Model Name": item["fine_tuned_model"],
+                        "Job ID": item["id"],
+                        "Model": item["model"],
+                        "Status": item["status"],
+                    }
+                )
+            st.table(table_data)
     elif button["name"] == "Delete a fine-tuned model":
-        model_names = [item["Model Name"] for item in parsed_output]
-        selected_model = st.selectbox("Select a model to delete:", model_names)
-
         if st.button(button["name"]):
-            if selected_model:
-                confirm_input = st.text_input("Are you sure? Type 'just do it' to confirm:")
-                if confirm_input == "just do it":
-                    command = f"{button['command']} {selected_model}"
-                    command_output = execute_command(command)
-                    terminal_output.text(command_output)
-                else:
-                    terminal_output.text("Deletion cancelled.")
-            else:
-                st.warning("Please select a model before deleting.")
+            model_names = [item["Model Name"] for item in parsed_output]
+            selected_model = st.selectbox("Select a Model Name", model_names)
+            confirm_message = st.text_input("Are you sure? Type 'just do it' to confirm.")
+            if confirm_message == "just do it":
+                command = f"openai --api-key {api_key} api models.delete -i {selected_model}"
+                command_output = execute_command(command)
+                st.text(f"Command Output: {command_output}")
+
