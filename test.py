df7955c5b8c1a793d0f998cabe49af6658311f97
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-18 18:55:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ api_key = st.text_input("Enter your OpenAI API KEY")
 # 定義 CLI 按鈕
 cli_buttons = [
     {"name": "List of all fine-tunes tasks", "command": f"openai --api-key {api_key} api fine_tunes.list"},
+    {"name": "Delete a fine-tuned model", "command": f"openai --api-key {api_key} api models.delete -i"}
 ]
 
 # 執行 CLI 指令
@@ -21,24 +22,13 @@ def parse_terminal_output(output):
     try:
         data = json.loads(output)
         rows = []
-        for item in data["data"]:
-            delete_button_key = f"delete_{item.get('id')}"
-            delete_button_label = f"Delete {item.get('fine_tuned_model')}"
-            delete_button_clicked = st.button(delete_button_label, key=delete_button_key)
-            if delete_button_clicked:
-                confirm_message = "Are you sure? Please enter 'just do it':"
-                user_input = st.text_input(confirm_message)
-                if user_input.strip() == "just do it":
-                    delete_command = f"openai --api-key {api_key} api models.delete -i {item.get('fine_tuned_model')}"
-                    delete_output = execute_command(delete_command)
-                    st.text(delete_output)
-            
+        for i, item in enumerate(data["data"], start=1):
             row = {
+                "No.": i,
                 "Model Name": item.get("fine_tuned_model"),
                 "Job ID": item.get("id"),
                 "Model": item.get("model"),
-                "Status": item.get("status"),
-                "Delete": delete_button_clicked
+                "Status": item.get("status")
             }
             rows.append(row)
         return rows
@@ -51,13 +41,26 @@ terminal_output = st.empty()
 
 # 監聽按鈕點擊事件
 for button in cli_buttons:
-    if st.button(button["name"]):
-        command_output = execute_command(button["command"])
-        terminal_output.text(command_output)
-        
-        if button["name"] == "List of all fine-tunes tasks":
-            parsed_output = parse_terminal_output(command_output)
+    if button["name"] == "List of all fine-tunes tasks":
+        if st.button(button["name"]):
+            command_output = execute_command(button["command"])
+            terminal_output.text(command_output)
             
-            # 顯示簡化的資訊表格
-            if parsed_output:
-                st.table(parsed_output)
+            parsed_output = parse_terminal_output(command_output)
+            st.table(parsed_output)
+
+    elif button["name"] == "Delete a fine-tuned model":
+        model_names = [item["Model Name"] for item in parsed_output]
+        selected_model = st.selectbox("Select a model to delete:", model_names)
+
+        if st.button(button["name"]):
+            if selected_model:
+                confirm_input = st.text_input("Are you sure? Type 'just do it' to confirm:")
+                if confirm_input == "just do it":
+                    command = f"{button['command']} {selected_model}"
+                    command_output = execute_command(command)
+                    terminal_output.text(command_output)
+                else:
+                    terminal_output.text("Deletion cancelled.")
+            else:
+                st.warning("Please select a model before deleting.")
