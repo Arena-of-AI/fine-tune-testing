5a01c686f5ca2d659ff0c7d21f6326c2ea78c47c
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-19 13:09:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import subprocess
 import json
-import pandas as pd
 
 # 輸入 OpenAI API KEY
 api_key = st.text_input("Enter your OpenAI API KEY")
 
 # 定義 CLI 按鈕
-cli_button = {"name": "List of all fine-tunes tasks", "command": f"openai --api-key {api_key} api fine_tunes.list"}
+cli_buttons = [
+    {"name": "List of all fine-tunes tasks", "command": f"openai --api-key {api_key} api fine_tunes.list"},
+    {"name": "Delete a fine-tuned model", "command": f"openai --api-key {api_key} api models.delete -i <FINE_TUNED_MODEL>"}
+]
 
 # 執行 CLI 指令
 def execute_command(command):
@@ -26,34 +28,33 @@ def parse_terminal_output(output):
                 "Job ID": item.get("id"),
                 "Model": item.get("model"),
                 "Status": item.get("status"),
-                "Delete": item.get("fine_tuned_model")
+                "Delete": st.checkbox(item.get("fine_tuned_model"))
             }
             rows.append(row)
-        return pd.DataFrame(rows)
+        return rows
     except Exception as e:
         st.error(f"Error parsing terminal output: {str(e)}")
-        return pd.DataFrame()
-
-# 刪除模型
-def delete_model(api_key, model_id):
-    command = f"openai --api-key {api_key} api models.delete -i {model_id}"
-    execute_command(command)
+        return []
 
 # 顯示終端輸出文本區域
 terminal_output = st.empty()
 
 # 監聽按鈕點擊事件
-if st.button(cli_button["name"]):
-    command_output = execute_command(cli_button["command"])
-    terminal_output.text(command_output)
-
-    if cli_button["name"] == "List of all fine-tunes tasks":
-        parsed_output = parse_terminal_output(command_output)
-        delete_buttons = parsed_output["Delete"].apply(lambda x: st.button(f"Delete {x}"))
-        parsed_output["Delete"] = delete_buttons
-        st.table(parsed_output)
-
-        # 檢查按鈕點擊事件
-        for index, row in parsed_output.iterrows():
-            if row["Delete"]:
-                delete_model(api_key, row['Model Name'])
+for button in cli_buttons:
+    if st.button(button["name"]):
+        command_output = execute_command(button["command"])
+        terminal_output.text(command_output)
+        
+        if button["name"] == "List of all fine-tunes tasks":
+            parsed_output = parse_terminal_output(command_output)
+            table = st.table(parsed_output)
+            
+            delete_model_indices = [i for i, row in enumerate(parsed_output) if row["Delete"]]
+            delete_model_ids = [parsed_output[i]["Model Name"] for i in delete_model_indices]
+            delete_model_button = st.button("Delete the model", key="delete_button", disabled=(len(delete_model_indices) == 0))
+            
+            if delete_model_button:
+                for model_id in delete_model_ids:
+                    delete_command = f"openai --api-key {api_key} api models.delete -i {model_id}"
+                    execute_command(delete_command)
+                st.success("Models deleted successfully.")
