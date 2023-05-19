d32b87b9389e9f3534e7f119767b5991607a1126
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-19 16:01:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,37 +2,56 @@ import streamlit as st
 import subprocess
 import json
 
-# 設置標題
+# 设置标题
 st.title("Check All Your Tasks")
 
-# 輸入 OpenAI API KEY
+# 输入 OpenAI API KEY
 api_key = st.text_input("Enter your OpenAI API KEY")
 
-# 定義 CLI 按鈕
+# 定义 CLI 按钮
 cli_buttons = [
     {"name": "List of all fine-tunes tasks", "command": f"openai --api-key {api_key} api fine_tunes.list"},
 ]
 
-# 執行 CLI 指令
+# 执行 CLI 指令
 def execute_command(command):
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output, _ = process.communicate()
+    output, error = process.communicate()
     return output.decode("utf-8")
 
-# 監聽按鈕點擊事件
+# 解析终端输出并显示简化的信息表格
+def parse_terminal_output(output):
+    try:
+        data = json.loads(output)
+        rows = []
+        for item in data["data"]:
+            row = {
+                "Model Name": item.get("fine_tuned_model"),
+                "Job ID": item.get("id"),
+                "Model": item.get("model"),
+                "Status": item.get("status")
+            }
+            rows.append(row)
+        return rows
+    except Exception as e:
+        st.error(f"Error parsing terminal output: {str(e)}")
+        return []
+    
+    
+# 监听按钮点击事件
 for button in cli_buttons:
     if st.button(button["name"]):
         command_output = execute_command(button["command"])
-        st.table(json.loads(command_output)["data"])
-
-        
-# 新增段落和按鈕
+        parsed_output = parse_terminal_output(command_output)
+        st.table(parsed_output)
+    
+# 新增段落和按钮
 st.title("Delete a Trained Model")
 st.text("Please input the model name you want to delete")
 model_name_input = st.text_input("Model Name:")
 delete_button = st.button("Delete this fine-tuned model")
 
-# 按鈕點擊事件
+# 按钮点击事件
 if delete_button:
     model_name = model_name_input.strip()
     if model_name:
