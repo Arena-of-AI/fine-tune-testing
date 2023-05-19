047dffcaaa9fee4996fb3d8ac768b5b04c43b9ce
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-19 13:29:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ api_key = st.text_input("Enter your OpenAI API KEY")
 # 定義 CLI 按鈕
 cli_buttons = [
     {"name": "List of all fine-tunes tasks", "command": f"openai --api-key {api_key} api fine_tunes.list"},
-    {"name": "Delete a fine-tuned model", "command": f"openai --api-key {api_key} api models.delete -i <FINE_TUNED_MODEL>"}
+    {"name": "Delete a fine-tuned model", "command": f"openai --api-key {api_key} api models.delete -i {st.text_input('Enter the model name to delete')}"},
 ]
 
 # 執行 CLI 指令
@@ -27,8 +27,7 @@ def parse_terminal_output(output):
                 "Model Name": item.get("fine_tuned_model"),
                 "Job ID": item.get("id"),
                 "Model": item.get("model"),
-                "Status": item.get("status"),
-                "Delete": False
+                "Status": item.get("status")
             }
             rows.append(row)
         return rows
@@ -39,34 +38,14 @@ def parse_terminal_output(output):
 # 顯示終端輸出文本區域
 terminal_output = st.empty()
 
-# 是否顯示表格
-show_table = False
-
 # 監聽按鈕點擊事件
 for button in cli_buttons:
+    if button["name"] == "Delete a fine-tuned model":
+        st.write("Please input the model name from the above table to delete it:")
     if st.button(button["name"]):
         command_output = execute_command(button["command"])
         terminal_output.text(command_output)
         
         if button["name"] == "List of all fine-tunes tasks":
-            show_table = True
-        
-        if button["name"] == "Delete a fine-tuned model":
-            show_table = False
-            
             parsed_output = parse_terminal_output(command_output)
-            delete_model_indices = [i for i, row in enumerate(parsed_output) if row["Delete"]]
-            delete_model_ids = [parsed_output[i]["Model Name"] for i in delete_model_indices]
-            
-            if len(delete_model_indices) > 0:
-                st.warning("Please select models to delete in the table.")
-            
-            for model_id in delete_model_ids:
-                delete_command = f"openai --api-key {api_key} api models.delete -i {model_id}"
-                execute_command(delete_command)
-            
-            st.success("Models deleted successfully.")
-    
-    if show_table:
-        parsed_output = parse_terminal_output(command_output)
-        table = st.table(parsed_output)
+            st.table(parsed_output)
