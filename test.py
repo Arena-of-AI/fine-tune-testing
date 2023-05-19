8ba3224210f1376f4c1158460d1fa0fc4dcb2a60
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-19 14:53:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,79 +2,65 @@ import streamlit as st
 import subprocess
 import json
 
-# 輸入 OpenAI API KEY
+# 设置标题
+st.title("Check All Your Tasks")
+
+# 输入 OpenAI API KEY
 api_key = st.text_input("Enter your OpenAI API KEY")
 
-# 定義 CLI 按鈕
+# 定义 CLI 按钮
 cli_buttons = [
     {"name": "List of all fine-tunes tasks", "command": f"openai --api-key {api_key} api fine_tunes.list"},
-    {"name": "Delete a fine-tuned model", "command": f"openai --api-key {api_key} api models.delete -i <FINE_TUNED_MODEL>"}
 ]
 
-# 執行 CLI 指令
+# 执行 CLI 指令
 def execute_command(command):
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     return output.decode("utf-8")
 
-# 解析終端資訊並顯示簡化的資訊表格
+# 解析终端输出并显示简化的信息表格
 def parse_terminal_output(output):
     try:
-        parsed_output = json.loads(output)
-        simplified_output = []
-        for item in parsed_output["data"]:
-            simplified_item = {
-                "Model Name": item["fine_tuned_model"],
-                "Job ID": item["id"],
-                "Model": item["model"],
-                "Status": item["status"]
+        data = json.loads(output)
+        rows = []
+        for item in data["data"]:
+            row = {
+                "Model Name": item.get("fine_tuned_model"),
+                "Job ID": item.get("id"),
+                "Model": item.get("model"),
+                "Status": item.get("status")
             }
-            simplified_output.append(simplified_item)
-        return simplified_output
-    except json.JSONDecodeError:
+            rows.append(row)
+        return rows
+    except Exception as e:
+        st.error(f"Error parsing terminal output: {str(e)}")
         return []
 
-# 顯示終端輸出表格
-def display_output_table(output):
-    if output:
-        st.table(output)
-    else:
-        st.write("No data to display.")
+# 监听按钮点击事件
+for button in cli_buttons:
+    if st.button(button["name"]):
+        command_output = execute_command(button["command"])
+        parsed_output = parse_terminal_output(command_output)
+        st.table(parsed_output)
 
-# 顯示模型名稱列表
-def display_model_names(model_names):
-    st.write("Model Names:")
-    for model_name in model_names:
-        st.write(model_name)
+# 新增段落和按钮
+st.title("Delete a Trained Model")
+st.text("Please input the model name you want to delete")
+model_name_input = st.text_input("Model Name:")
+delete_button = st.button("Delete this fine-tuned model")
 
-# 顯示刪除模型的表單
-def display_delete_form():
-    st.subheader("Delete a Trained Model")
-    model_name_input = st.text_input("Please input the model name you want to delete")
-    delete_button = st.button("Delete this fine-tuned model")
-
-    if delete_button:
-        if model_name_input in model_names:
-            command = f"openai --api-key {api_key} api models.delete -i {model_name_input}"
-            execute_command(command)
-            st.success("Model deleted successfully.")
+# 按钮点击事件
+if delete_button:
+    model_name = model_name_input.strip()
+    if model_name:
+        model_names = [item["Model Name"] for item in parsed_output] if "parsed_output" in locals() else []
+        model_names_full = [name for name in model_names if model_name in name]
+        if model_names_full:
+            delete_command = f"openai --api-key {api_key} api models.delete -i {model_names_full[0]}"
+            delete_output = execute_command(delete_command)
+            st.text(delete_output)
         else:
             st.error("Please enter a valid model name.")
-
-# 主程式
-def main():
-    st.title("Check All Your Tasks")
-    st.subheader("List of all fine-tunes tasks")
-    
-    for button in cli_buttons:
-        if st.button(button["name"]):
-            command_output = execute_command(button["command"])
-            simplified_output = parse_terminal_output(command_output)
-            display_output_table(simplified_output)
-            model_names = [item["Model Name"] for item in simplified_output]
-            display_model_names(model_names)
-    
-    display_delete_form()
-
-if __name__ == "__main__":
-    main()
+    else:
+        st.error("Please enter a model name.")
