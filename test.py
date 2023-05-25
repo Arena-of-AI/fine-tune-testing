75605228c1a56050dbcc7ec4deda39ad7fb66a33
Arena-of-AI <132893291+Arena-of-AI@users.noreply.github.com>
2023-05-25 11:40:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -85,8 +85,6 @@ if delete_button:
                 st.error("Deletion Failed")
         
     else:
-        # 如果模型名称为空白，则设置 show_table 为 True，并显示原有的表格数据
-        session_state.show_table = True
         if session_state.show_table:
            st.table(session_state.data)
 
